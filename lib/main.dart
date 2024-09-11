a8ae8840a9f2d94ee4eef5a1338e3c53b609148a
HIHIYAYAYOO <145166934+HIHHIYAYAYOO@users.noreply.github.com>
2024-09-12 02:04:31 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,89 +49,84 @@ class _SearchPageState extends State<SearchPage> {
   List<dynamic> resultList = []; // 用於存儲解析的結果列表
   String responseText = "";
 
-Future<void> search() async {
-  String token = tokenController.text;
-  String schoolId = schoolIdController.text;
-  String year = yearController.text;
-  String course = courseController.text;
-  String department = departmentController.text;
-  String teacher = teacherController.text;
-  String hard = hardController.text;
-  String ip = ipController.text;
-  String limit = limitController.text;
-  String recommend = recommendController.text;
+  Future<void> search() async {
+    String token = tokenController.text;
+    String schoolId = schoolIdController.text;
+    String year = yearController.text;
+    String course = courseController.text;
+    String department = departmentController.text;
+    String teacher = teacherController.text;
+    String hard = hardController.text;
+    String recommend = recommendController.text;
 
-  var url = Uri.parse('https://tewkr.com/api/search');
-  var headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6",
-  };
+    var url = Uri.parse('https://tewkr.com/api/search');
+    var headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Encoding": "gzip, deflate, br, zstd",
+      "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6",
+    };
 
-  var data = jsonEncode({
-    "token": token.isEmpty ? "" : token,
-    "school_id": schoolId.isEmpty ? 17 : int.parse(schoolId),
-    "year": year.isEmpty ? "" : year,
-    "course": course.isEmpty ? "" : course,
-    "department": department.isEmpty ? "" : department,
-    "teacher": teacher.isEmpty ? "" : teacher,
-    "hard": hard.isEmpty ? "" : hard,
-    "ip": ip.isEmpty ? "" : ip,
-    "limit": limit.isEmpty ? 500 : int.parse(limit),
-    "recommend": recommend.isEmpty ? "" : recommend,
-  });
+    var data = jsonEncode({
+      "token": token.isEmpty ? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmFtZSI6IjQxMTAyNTAzMCIsImlhdCI6MTcyNjA1MTA3NH0.K0sBIhgRV53EuDRCxvZiBiDo_r_lgb7fZfiikB6ag70" : token,
+      "school_id": schoolId.isEmpty ? 17 : int.parse(schoolId),
+      "year": year.isEmpty ? "" : year,
+      "course": course.isEmpty ? "" : course,
+      "department": department.isEmpty ? "" : department,
+      "teacher": teacher.isEmpty ? "" : teacher,
+      "hard": hard.isEmpty ? "" : hard,
+      "recommend": recommend.isEmpty ? "" : recommend,
+    });
 
-  try {
-    var response = await http.post(url, headers: headers, body: data);
+    try {
+      var response = await http.post(url, headers: headers, body: data);
 
-    if (response.statusCode == 200) {
-      // 檢查是否使用 Brotli 編碼
-      if (response.headers['content-encoding'] == 'br') {
-        Uint8List decompressed = Uint8List.fromList(brotli.decode(response.bodyBytes));
-        String decodedData = utf8.decode(decompressed);
-        setState(() {
-          var jsonResponse = jsonDecode(decodedData);
-          if (jsonResponse['code'] == 200) {
-            // 過濾掉廣告
-            resultList = (jsonResponse['result'] as List<dynamic>)
-                .where((item) => item['ad'] == 0)
-                .toList();
-          } else {
-            responseText = jsonResponse['msg'];
-            resultList = [];
-          }
-        });
-      } else {
-        var jsonResponse = jsonDecode(response.body);
-        if (jsonResponse['code'] == 200) {
-          // 過濾掉廣告
+      if (response.statusCode == 200) {
+        // 檢查是否使用 Brotli 編碼
+        if (response.headers['content-encoding'] == 'br') {
+          Uint8List decompressed = Uint8List.fromList(brotli.decode(response.bodyBytes));
+          String decodedData = utf8.decode(decompressed);
           setState(() {
-            resultList = (jsonResponse['result'] as List<dynamic>)
-                .where((item) => item['ad'] == 0)
-                .toList();
+            var jsonResponse = jsonDecode(decodedData);
+            if (jsonResponse['code'] == 200) {
+              // 過濾掉廣告
+              resultList = (jsonResponse['result'] as List<dynamic>)
+                  .where((item) => item['ad'] == 0)
+                  .toList();
+            } else {
+              responseText = jsonResponse['msg'];
+              resultList = [];
+            }
           });
         } else {
-          setState(() {
-            responseText = jsonResponse['msg'];
-            resultList = [];
-          });
+          var jsonResponse = jsonDecode(response.body);
+          if (jsonResponse['code'] == 200) {
+            // 過濾掉廣告
+            setState(() {
+              resultList = (jsonResponse['result'] as List<dynamic>)
+                  .where((item) => item['ad'] == 0)
+                  .toList();
+            });
+          } else {
+            setState(() {
+              responseText = jsonResponse['msg'];
+              resultList = [];
+            });
+          }
         }
+      } else {
+        setState(() {
+          responseText = '請求失敗，狀態碼: ${response.statusCode}';
+          resultList = [];
+        });
       }
-    } else {
+    } catch (e) {
       setState(() {
-        responseText = '請求失敗，狀態碼: ${response.statusCode}';
+        responseText = '請求發生錯誤: $e';
         resultList = [];
       });
     }
-  } catch (e) {
-    setState(() {
-      responseText = '請求發生錯誤: $e';
-      resultList = [];
-    });
   }
-}
-
 
   String formatTimestamp(String timestamp) {
     DateTime dateTime = DateTime.parse(timestamp);
@@ -151,18 +146,26 @@ Future<void> search() async {
             padding: const EdgeInsets.all(16.0),
             sliver: SliverList(
               delegate: SliverChildListDelegate([
-                buildTextField(courseController, '課程名稱', '輸入課程名稱'),
-                buildTextField(teacherController, '教師名稱', '輸入教師名稱'),
                 buildTextField(yearController, '開課年份', '13-2'),
                 buildTextField(departmentController, '課程領域', '認列校核心課程與教學學系'),
-                buildTextField(recommendController, '推薦', 'A、B、C、D、E'),
-                buildTextField(hardController, '難度', 'A、B、C、D、E'),
+                buildTextField(courseController, '課程名稱', '輸入課程名稱'),
+                buildTextField(teacherController, '教師名稱', '輸入教師名稱'),
+                Row(
+                  children: [
+                    Flexible(
+                      child: buildTextField(recommendController, '推薦', 'A、B、C、D、E'),
+                    ),
+                    const SizedBox(width: 16),
+                    Flexible(
+                      child: buildTextField(hardController, '難度', 'A、B、C、D、E'),
+                    ),
+                  ],
+                ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: search,
                   child: const Text('搜尋'),
                 ),
-                const SizedBox(height: 20),
                 if (responseText.isNotEmpty)
                   Text(
                     responseText,
