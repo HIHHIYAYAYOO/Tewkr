df6a0b1cba6c23439d990e89e6fffb61b623ab5d
HIHIYAYAYOO <145166934+HIHHIYAYAYOO@users.noreply.github.com>
2024-09-12 23:56:59 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter HTTP POST Example',
+      title: 'Tewkr NDHU',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
