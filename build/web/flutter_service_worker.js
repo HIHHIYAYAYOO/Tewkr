6b530d2b982fdb02132270f2f22bf9e77bc8e71a
HIHIYAYAYOO <145166934+HIHHIYAYAYOO@users.noreply.github.com>
2024-12-07 16:19:42 +0800

    update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c357c3a7e3fe6650e8cc7104c39de1d9",
+"flutter_bootstrap.js": "29b16495f9697bf3acb574fa7c6a62cd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
