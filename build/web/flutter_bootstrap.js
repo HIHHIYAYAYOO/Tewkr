be1c8747c5d42836ac2a113e371f5951ecb0e9c5
HIHIYAYAYOO <145166934+HIHHIYAYAYOO@users.noreply.github.com>
2024-12-07 16:44:46 +0800

    update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2480907995"
+    serviceWorkerVersion: "230545561"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "e3b0e86d4a8befa31e5140d8f97bcf71",
+"flutter_bootstrap.js": "0046d90bf4b389f77cfffaa025894dad",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
